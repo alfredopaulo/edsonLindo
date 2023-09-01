fa13e41d755b6e5eadbb2cdbd9c7cdbf5dd831a3
Alfredo Paulo <alfredopaulo2003@gmail.com>
2023-08-31 21:50:56 -0400

    💕 feat: add new images

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,8 @@ class MyApp extends StatelessWidget {
             'images/edson3.jpg',
             'images/edson4.jpg',
             'images/edson5.jpg',
+            'images/edson6.jpg',
+            'images/edson7.jpg',
             // Adicione mais caminhos para outras imagens...
           ]),
         ),
